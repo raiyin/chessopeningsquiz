d0d142bc6d88420b6ef396f70d48dafc28681ab3
Lomo <my@test.com>
2022-09-03 00:20:22 +0300

    Settings font color problem fixed

--- app/src/main/java/com/publicmaders/android/chessopeningsquiz/activities/CoordconfigActivity.kt
+++ app/src/main/java/com/publicmaders/android/chessopeningsquiz/activities/CoordconfigActivity.kt
@@ -12,7 +12,6 @@ class CoordconfigActivity : AppCompatActivity()
 {
     private lateinit var rbNoCoord: RadioButton
     private lateinit var rbCoordInside: RadioButton
-    //private lateinit var rbCoordOutside: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -21,15 +20,12 @@ class CoordconfigActivity : AppCompatActivity()
 
         rbNoCoord = findViewById(R.id.rbDontShowCoord)
         rbCoordInside = findViewById(R.id.rbShowCoordInside)
-        //rbCoordOutside = findViewById(R.id.rbShowCoordOutside)
 
         Settings.load(applicationContext)
         when (Settings.CoordMode)
         {
             CoordinatesMode.NO -> rbNoCoord.isChecked = true
             else -> rbCoordInside.isChecked = true
-            //CoordinatesMode.INSIDE -> rbCoordInside.isChecked = true
-            //else -> rbCoordOutside.isChecked = true
         }
 
         rbNoCoord.setOnCheckedChangeListener { _, isChecked ->
@@ -47,13 +43,5 @@ class CoordconfigActivity : AppCompatActivity()
                 Settings.save(applicationContext)
             }
         }
-
-        /*rbCoordOutside.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked)
-            {
-                Settings.CoordMode = CoordinatesMode.OUTSIDE
-                Settings.save(applicationContext)
-            }
-        }*/
     }
 }
